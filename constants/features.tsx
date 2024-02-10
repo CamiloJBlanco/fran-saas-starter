46a3e9a70a19ebd23c4125c50dc8010efe1847bd
Camilo <cblancodelavega@gmail.com>
2024-02-10 01:09:32 -0300

    add features

---
+++ constants/features.tsx
@@ -0,0 +1,36 @@
+export const FeaturesInfo = [
+    {
+        imageUrl: '/grid-icons/asset-1.svg',
+        title: 'Where are we located?',
+        description:
+            'Rioja 1254 Of. 306, S2000 AYR, Santa Fe, Argentina',
+    },
+    {
+        imageUrl: '/grid-icons/asset-2.svg',
+        title: 'Full-Time Support',
+        description:
+            "Our support team is available 24/7 to assist with any questions or issues you have. Fran provides prompt and reliable assistance for technical problems or using our services."
+    },
+    {
+        imageUrl: '/grid-icons/asset-3.svg',
+        title: 'Multiplatform support',
+        description: "Enjoy a seamless experience across all your devices. Whether you're using a desktop, smartphone, or tablet.",
+    },
+    {
+        imageUrl: '/grid-icons/asset-4.svg',
+        title: 'Comprehensive support options',
+        description:
+            "Our support services are available via WhatsApp, website, phone, and email, providing prompt assistance in your preferred method.",
+    },
+    {
+        imageUrl: '/grid-icons/asset-5.svg',
+        title: 'Robust security measures',
+        description: "Our platform ensures your data's security with advanced measures like end-to-end encryption, regular audits, and proactive monitoring. Trust Fran to keep your information safe."
+    },
+    {
+        imageUrl: '/grid-icons/asset-6.svg',
+        title: 'Accessibility for all',
+        description: "Our platform prioritizes accessibility for all users, ensuring easy navigation and use. Fran provides intuitive interfaces and supports assistive technologies for an inclusive experience."
+    }
+
+];
